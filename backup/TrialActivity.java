8b6f358742f78032d7e6eef6b91c9a03b61c92f5
Shaunak Das <shaunakdas2020@gmail.com>
2017-11-27 18:46:20 +0530

    first draft

---
+++ backup/TrialActivity.java
@@ -0,0 +1,14 @@
+package line.ngui.trial;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class TrialActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trial);
+    }
+
+}
